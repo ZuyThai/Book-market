3e4de1ffc7caeab1a4fb7398e98f59eb9fb28a74
Thai Bui <buithaitb911@gmail.com>
2023-06-03 21:34:32 +0700

    my first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import { PUBLIC_ROUTER, PRIVATE_ROUTER } from "./router/router";
+import ProtectRouter from "./router/ProtectRouter";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { fetchUser } from "./redux/Slices/AuthSlice";
+import { useDispatch } from "react-redux";
+import { TOKEN_USER } from "./constant";
+
+function App() {
+  const dispatch = useDispatch();
+  useLayoutEffect(() => {
+    if (localStorage.getItem(TOKEN_USER)) {
+      dispatch(fetchUser());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {PUBLIC_ROUTER.map((publicRouter, index) => {
+            const Comp = publicRouter.component;
+            return (
+              <Route
+                key={index}
+                path={publicRouter.path}
+                element={<Comp />}
+              ></Route>
+            );
+          })}
+
+          {PRIVATE_ROUTER.map((privateRouter, index) => {
+            const Comp = privateRouter.component;
+            return (
+              <Route
+                key={index}
+                path={privateRouter.path}
+                element={
+                  <ProtectRouter>
+                    <Comp />
+                  </ProtectRouter>
+                }
+              ></Route>
+            );
+          })}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
