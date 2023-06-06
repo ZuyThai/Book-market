541cd656c3e565a6fdf76878d608df88da90f383
Thai Bui <buithaitb911@gmail.com>
2023-06-06 20:37:56 +0700

    add author field

--- src/models/Product.js
+++ src/models/Product.js
@@ -7,6 +7,7 @@ const productSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    author: {type: String},
     image: {type: String},
     cloudinary_id: {type: String}
 }, {timestamps: true})
